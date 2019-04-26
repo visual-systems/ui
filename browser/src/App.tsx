d5f9012770f874ab456c8e5c0e4e875c751420d3
Lyndon Maydwell <lyndon@sordina.net>
2019-04-26 16:01:35 +1000

    panes

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -2,24 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import SplitterLayout from 'react-splitter-layout';
+import 'react-splitter-layout/lib/index.css';
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitterLayout> 
+      <div>
+        <h2>Navigation</h2>
+      </div>
+      <SplitterLayout vertical>
+        <div className="App">
+          <h2>Graph</h2>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+        <div>
+          <h2>Properties</h2>
+        </div>
+      </SplitterLayout>
+    </SplitterLayout>
   );
 }
 
